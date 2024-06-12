27a1f99b783761661f8647ef9627d545973a3c7b
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2024-06-11 22:22:27 -0400

    phone removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Footer() {
   return (
     <div className='feet'>
-        <a href='tel: 7029342205'><h3>Phone</h3></a>
         <a href='mailto:2022leonvargase@cornerstoneprep.net'><h3>Email</h3></a>
         <a href='http://www.linkedin.com/in/emanuel-leon-vargas-751720296' target="_blank"><h3>LinkedIn</h3></a>
         <a href='https://github.com/EmanuelLeon2022' target="_blank"><h3>GitHub</h3></a>
